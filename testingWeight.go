e706eb3a525d5ed0d96854f64a3beefad6993519
tiagov07 <santiago.valencia@mercadolibre.com.co>
2022-03-17 17:31:19 -0500

    recomendation

--- testingWeight.go
+++ testingWeight.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var nombre string
+	var name string
 	var weight float32
-	fmt.Println("compartenos tu nombre")
-	fmt.Scanf("%s", &nombre)
+	fmt.Println("compartenos tu name")
+	fmt.Scanf("%s", &name)
 	fmt.Println("compartenos tu peso")
 	fmt.Scanf("%f", &weight)
 	if weight > 80 {
 		weight = weight - 8.0
-		fmt.Printf("estas empezando a tener exceso de peso %v \n te recomendamos bajar al menos a %v kg", nombre, weight)
+		fmt.Printf("estas empezando a tener exceso de peso %v \n te recomendamos bajar al menos a %v kg", name, weight)
 	}
 
 }
